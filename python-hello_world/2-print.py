f86b4ad784c774623693c0d15929520b5768a947
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-05-06 20:29:15 +0000

    print

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 # Using 'print()' to print a frase in the console
-print("\"Programming is like building a multilingual puzzle\n")
+print('"Programming is like building a multilingual puzzle')
