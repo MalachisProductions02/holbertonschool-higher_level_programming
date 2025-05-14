c7deb410979b16ec908af73d9a235795f775e645
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-05-14 23:26:35 +0000

    square matrix simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Usamos comprensión de listas para crear una nueva matriz con los valores al cuadrado
-    return [[x ** 2 for x in row] for row in matrix]
+    return [[x ** 2 for x in row]
+            for row in matrix]
