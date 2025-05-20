cfaed7d0c21361477dd612b2395b6cdd13e196f3
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-05-20 23:32:17 +0000

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class Square with a private instance attribute."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize a new square with a private size attribute."""
+        self.__size = size
