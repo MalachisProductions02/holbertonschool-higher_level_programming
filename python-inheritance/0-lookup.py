6c757abbeeec5a8f91941501f1404a876f9c1dda
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-05-26 23:51:54 +0000

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Este módulo contiene la función 'lookup' que retorna los atributos y métodos disponibles de un objeto."""
+"""
+Este módulo contiene la función 'lookup' que retorna
+los atributos y métodos disponibles de un objeto.
+"""
+
 
 def lookup(obj):
     """
