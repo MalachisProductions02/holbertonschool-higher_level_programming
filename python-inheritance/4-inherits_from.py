0fc38d852751eeec89819969c459dcd78d6e11fb
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-05-27 00:30:10 +0000

    inherits from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Function that checks if an object is an instance of a subclass of a_class."""
+
 
 def inherits_from(obj, a_class):
-    """Return True if obj is instance of a subclass (not exact class) of a_class."""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    """
+    Return True if obj is an instance of a subclass (not exact class) of a_class.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
