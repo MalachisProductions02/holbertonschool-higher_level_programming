2fc1cb9426c81ec2989507ab43053c47de6f6d99
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-06-02 23:42:29 +0000

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python3}
+"""
+This module provides a function to read and print the contents of a UTF-8 text file.
+"""
+
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
