70e1b7ca896bf81aa4aff9a93ce8d86e89e3dc0b
Daniel Farfán <malachisproductionsnooficial@gmail.com>
2025-06-24 23:46:44 -0600

    tv shows

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- List shows with their genre IDs, sorted by show title and genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genre ON tv_shows.id = tv_show_genre.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
