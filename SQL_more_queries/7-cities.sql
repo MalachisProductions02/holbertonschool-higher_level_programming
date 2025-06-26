20c85a5124fbb966c5270a923ade6db98512b575
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-06-26 02:47:48 +0000

    cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,4 @@
 -- Creates the database hbtn_0d_usa and the table cities with a foreign key to states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
