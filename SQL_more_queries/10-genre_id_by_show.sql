103e74b10fef041996595bbf170c52165b0953cc
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-06-26 02:20:57 +0000

    INNER JOIN

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,4 @@
 -- List shows with their genre IDs, sorted by show title and genre_id
-USE hbtn_0d_tvshows;
-
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
