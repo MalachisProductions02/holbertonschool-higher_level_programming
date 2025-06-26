e4423eee747bcd0dd3ebe818d932d87048f074e7
Daniel Farfán <malachisproductionsnooficial@gmail.com>
2025-06-25 19:33:31 -0600

    INNER JOIN

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- List shows with their genre IDs, sorted by show title and genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows_genres
+FROM hbtn_0d_tvshows
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
