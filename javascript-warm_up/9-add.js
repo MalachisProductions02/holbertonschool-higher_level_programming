b0ac537f19be594c65c5da58aeb644337ed1e9f2
MalachisProductions02 <malachisproductionsnooficial@gmai.com>
2025-07-12 21:38:11 +0000

    task09

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-function add(a, b) {
+function add (a, b) {
     return a + b;
   }
   
@@ -9,5 +9,5 @@ function add(a, b) {
   const num1 = parseInt(arg1, 10);
   const num2 = parseInt(arg2, 10);
   
-  console.log(add(num1, num2));
+  console.log(add(num1, num2));  
   
